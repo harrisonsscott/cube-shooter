c5029202cfdbf59913b2103319b9b03dc282e168
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-02-25 09:57:38 -0800

    Added a global functions script along with an abbreviation function

---
+++ Assets/Scripts/GlobalFunctions.cs
@@ -0,0 +1,19 @@
+using System;
+
+// functions that can be access by all the scripts
+
+public static class GlobalFunctions {
+    private static string[] abbreviations = { "", "k", "m", "b", "t", "q", "qu", "s", "o", "n" };
+    public static string abbreviate(long number){ // abbreviates a number, ex 1,000 -> 1K
+        int index = 0;
+        double num = number;
+
+        while (num >= 1000){
+            num = Math.Floor(num)/1000;
+            index += 1;
+        }
+        
+        return num + abbreviations[index];
+        
+    }
+}
